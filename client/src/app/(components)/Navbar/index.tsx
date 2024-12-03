a55ff03705c1716f236010533176bdca29d9914b
SrujanRaghavendraS <srujan9712@gmail.com>
2024-12-03 21:25:05 +0530

    Images added

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -2,6 +2,7 @@
 import { useAppDispatch, useAppSelector } from "@/app/redux";
 import { setisdarkMode, setIsSidebarCollapsed } from "@/app/state";
 import { Bell, Menu, Moon, Settings, Sun } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 const Navbar = () => {
   const dispatch = useAppDispatch();
@@ -52,9 +53,8 @@ const Navbar = () => {
           </div>
           <hr className="w-0 h-7 border-solid border-l border-gray-300 mx-3" />
           <div className="flex items-center gap-3 cursor-pointer">
-            <div className="w-9 h-9">
-              image
-            </div>
+            <Image src = "https://s3-inventorymanagement-srujan.s3.ap-south-1.amazonaws.com/profile.jpg"
+            alt="profile" width={50} height={50} className="rounded-full h-full object-cover"/>
             <span className="font-semibold">Srujan</span>
           </div>
         </div>
--- client/src/app/dashboard/CardPopularProduct.tsx
+++ client/src/app/dashboard/CardPopularProduct.tsx
@@ -2,6 +2,7 @@ import { useGetDashboardMetricsQuery } from "@/app/state/api";
 import React from "react";
 import { ShoppingBag } from "lucide-react";
 import Rating from "../(components)/Rating";
+import Image from "next/image";
 const CardPopularProducts = () => {
   const { data: dashboardMetrics, isLoading } = useGetDashboardMetricsQuery();
 
@@ -22,7 +23,8 @@ const CardPopularProducts = () => {
                 className="flex items-center justify-between gap-3 px-5 py-7 border-b"
               >
                 <div className="flex items-center gap-3">
-                  <div>Image</div>
+                <Image src = {`https://s3-inventorymanagement-srujan.s3.ap-south-1.amazonaws.com/product${Math.floor(Math.random()*3)+1}.png`}
+            alt={product.name} width={48} height={48} className="rounded-lg w-14 h-14"/>
                   <div className="flex flex-col justify-between gap-1">
                     <div className="font-bold text-gray-700">
                       {product.name}
